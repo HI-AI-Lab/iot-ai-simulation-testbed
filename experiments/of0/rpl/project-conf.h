0c5e5edb7dd6c64f61d52838936749fb156c9883
Mufrah Naveed <mufrah.naveed.mughal@gmail.com>
2025-07-29 18:51:44 +0500

    Update project-conf.h
    another try

--- experiments/of0/rpl/project-conf.h
+++ experiments/of0/rpl/project-conf.h
@@ -1,30 +1,28 @@
 #ifndef PROJECT_CONF_H_
 #define PROJECT_CONF_H_
 
-/* Disable RPL Lite */
-#undef UIP_CONF_ROUTER
-#define UIP_CONF_ROUTER              1
-
-#undef NETSTACK_CONF_ROUTING
-#define NETSTACK_CONF_ROUTING        rpl_classic_driver
-
-#undef RPL_CONF_OF
-#define RPL_CONF_OF                  rpl_of0
-
-#undef RPL_CONF_RPL_CLASSIC
-#define RPL_CONF_RPL_CLASSIC         1
-
-#undef UIP_CONF_ND6_SEND_RA
-#define UIP_CONF_ND6_SEND_RA         0
-
-/* Logging */
+/* Disable compact address logging */
 #ifdef LOG_WITH_COMPACT_ADDR
 #undef LOG_WITH_COMPACT_ADDR
 #endif
-#define LOG_WITH_COMPACT_ADDR        0
+#define LOG_WITH_COMPACT_ADDR 0
 
-#define LOG_CONF_LEVEL_RPL           LOG_LEVEL_INFO
-#define LOG_CONF_LEVEL_IPV6          LOG_LEVEL_WARN
-#define LOG_CONF_LEVEL_TCPIP         LOG_LEVEL_WARN
+/* Enable RPL Classic with OF0 */
+#undef NETSTACK_CONF_ROUTING
+#define NETSTACK_CONF_ROUTING rpl_classic_driver
+
+#undef RPL_CONF_OF
+#define RPL_CONF_OF rpl_of0
+
+#undef RPL_CONF_RPL_CLASSIC
+#define RPL_CONF_RPL_CLASSIC 1
+
+#undef UIP_CONF_ND6_SEND_RA
+#define UIP_CONF_ND6_SEND_RA 0
+
+/* Logging levels */
+#define LOG_CONF_LEVEL_RPL   LOG_LEVEL_INFO
+#define LOG_CONF_LEVEL_IPV6  LOG_LEVEL_WARN
+#define LOG_CONF_LEVEL_TCPIP LOG_LEVEL_WARN
 
 #endif /* PROJECT_CONF_H_ */
