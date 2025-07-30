84e9f7f8da7673c71ef6fee547098e54e6469933
Mian Muhammad Mubasher <mian.muhammad.mubasher@gmail.com>
2025-07-30 23:24:49 +0500

    Update project-conf.h
    update

--- experiments/of0/rpl/project-conf.h
+++ experiments/of0/rpl/project-conf.h
@@ -1,13 +1,9 @@
 #ifndef PROJECT_CONF_H_
 #define PROJECT_CONF_H_
 
-/* Disable RPL Lite explicitly */
-#undef RPL_CONF_RPL_LITE
-#define RPL_CONF_RPL_LITE 0
-
-/* Use RPL Classic with OF0 */
+/* Set RPL Lite as default */
 #undef NETSTACK_CONF_ROUTING
-#define NETSTACK_CONF_ROUTING rpl_classic_driver
+#define NETSTACK_CONF_ROUTING rpl_lite_driver
 
 #undef RPL_CONF_OF
 #define RPL_CONF_OF rpl_of0
@@ -15,7 +11,7 @@
 #undef UIP_CONF_ND6_SEND_RA
 #define UIP_CONF_ND6_SEND_RA 0
 
-/* Optional: Enable logging and stats */
+/* Logging levels */
 #define LOG_CONF_LEVEL_RPL   LOG_LEVEL_INFO
 #define LOG_CONF_LEVEL_IPV6  LOG_LEVEL_WARN
 #define LOG_CONF_LEVEL_TCPIP LOG_LEVEL_WARN
