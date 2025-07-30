2b02846388802cf513fa78c063867bbe311fc944
Mian Muhammad Mubasher <mian.muhammad.mubasher@gmail.com>
2025-07-30 21:39:30 +0500

    Update project-conf.h
    update

--- experiments/of0/rpl/project-conf.h
+++ experiments/of0/rpl/project-conf.h
@@ -1,21 +1,18 @@
 #ifndef PROJECT_CONF_H_
 #define PROJECT_CONF_H_
 
-/* Disable compact address logging */
+/* Compact address logging override */
 #ifdef LOG_WITH_COMPACT_ADDR
 #undef LOG_WITH_COMPACT_ADDR
 #endif
 #define LOG_WITH_COMPACT_ADDR 0
 
-/* Enable RPL Classic with OF0 */
+/* Set RPL Classic with OF0 */
 #undef NETSTACK_CONF_ROUTING
 #define NETSTACK_CONF_ROUTING rpl_classic_driver
 
 #undef RPL_CONF_OF
 #define RPL_CONF_OF rpl_of0
-
-#undef RPL_CONF_RPL_CLASSIC
-#define RPL_CONF_RPL_CLASSIC 1
 
 #undef UIP_CONF_ND6_SEND_RA
 #define UIP_CONF_ND6_SEND_RA 0
