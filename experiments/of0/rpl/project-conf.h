02a7aaf50ad5b78cb27fc60e357b0d1b8226c358
Mufrah Naveed <mufrah.naveed.mughal@gmail.com>
2025-07-30 22:26:56 +0500

    Update project-conf.h
    update

--- experiments/of0/rpl/project-conf.h
+++ experiments/of0/rpl/project-conf.h
@@ -1,21 +1,32 @@
 #ifndef PROJECT_CONF_H_
 #define PROJECT_CONF_H_
 
-/* Compact address logging override */
-#ifdef LOG_WITH_COMPACT_ADDR
+/* Logging */
 #undef LOG_WITH_COMPACT_ADDR
-#endif
 #define LOG_WITH_COMPACT_ADDR 0
 
-/* Set RPL Classic with OF0 */
+/* Use RPL Classic instead of RPL Lite */
+#undef UIP_CONF_ROUTER
+#define UIP_CONF_ROUTER 1
+
 #undef NETSTACK_CONF_ROUTING
 #define NETSTACK_CONF_ROUTING rpl_classic_driver
+
+#undef UIP_CONF_ND6_SEND_RA
+#define UIP_CONF_ND6_SEND_RA 0
 
 #undef RPL_CONF_OF
 #define RPL_CONF_OF rpl_of0
 
-#undef UIP_CONF_ND6_SEND_RA
-#define UIP_CONF_ND6_SEND_RA 0
+#undef RPL_CONF_STATS
+#define RPL_CONF_STATS 1
+
+#undef RPL_CONF_WITH_DAO_ACK
+#define RPL_CONF_WITH_DAO_ACK 1
+
+/* Disable RPL Lite */
+#undef RPL_CONF_RPL_LITE
+#define RPL_CONF_RPL_LITE 0
 
 /* Logging levels */
 #define LOG_CONF_LEVEL_RPL   LOG_LEVEL_INFO
