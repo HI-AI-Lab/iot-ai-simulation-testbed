fa8b1b544e04ef02c619cc1ca354f2361232c04d
Mufrah Naveed <mufrah.naveed.mughal@gmail.com>
2025-08-07 02:23:46 +0500

    Update simulation2.js
    seeking a stable version

--- experiments/of0/rpl/simulation2.js
+++ experiments/of0/rpl/simulation2.js
@@ -1,7 +1,5 @@
-log.log("Simulation started\n");
 TIMEOUT(60000); // Schedule a stop at 60 seconds
 while (time < 60000) {
   YIELD();
 }
-log.log("Simulation ended at: " + time + "\n");
 log.testOK();
