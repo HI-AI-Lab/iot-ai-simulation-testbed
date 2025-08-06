3a9ec327c753b7c28fdab4919833bbbdec7267b3
Mufrah Naveed <mufrah.naveed.mughal@gmail.com>
2025-08-07 01:17:05 +0500

    Update simulation2.js
    fixed script

--- experiments/of0/rpl/simulation2.js
+++ experiments/of0/rpl/simulation2.js
@@ -1,11 +1,3 @@
-log.log("Simulation started\n");
-
-// Optional: log when simulation time is up
 TIMEOUT(60000);
-
-while (time < 60000) {
-  YIELD(); // Keeps control in simulation until 60 seconds
-}
-
-log.log("Simulation reached 60 seconds. Ending.\n");
-log.testOK(); // Must be called before simulation stops
+while (time < 60000) YIELD();
+log.testOK();
