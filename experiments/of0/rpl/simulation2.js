016210d57a999214ac3d5e891cf10f4b2b6227e5
Mufrah Naveed <mufrah.naveed.mughal@gmail.com>
2025-08-07 02:28:45 +0500

    Update simulation2.js
    minimal script

--- experiments/of0/rpl/simulation2.js
+++ experiments/of0/rpl/simulation2.js
@@ -1,9 +1,1 @@
-TIMEOUT(60000), log.testOK()); // Schedule a stop at 60 seconds
-/*
-while (true) {
-  YIELD();
-  if(msg){
-    log.log("mote output: " + msg + "\n");
-  }
-}
-*/
+TIMEOUT(60000, log.testOK());
