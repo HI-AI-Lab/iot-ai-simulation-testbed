ff483e289e6662aaa7237cc340bd693f546d3d39
Mian Muhammad Mubasher <mian.muhammad.mubasher@gmail.com>
2025-08-07 02:25:55 +0500

    Update simulation2.js
    fixed script

--- experiments/of0/rpl/simulation2.js
+++ experiments/of0/rpl/simulation2.js
@@ -1,5 +1,7 @@
-TIMEOUT(60000); // Schedule a stop at 60 seconds
+TIMEOUT(60000), log.testOK()); // Schedule a stop at 60 seconds
 while (time < 60000) {
   YIELD();
+  if(msg){
+    log.log("mote output: " + msg + "\n");
+  }
 }
-log.testOK();
