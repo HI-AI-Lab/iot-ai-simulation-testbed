f5483b9cfce12aef21922d5e5c7c412d7d14be05
Mian Muhammad Mubasher <mian.muhammad.mubasher@gmail.com>
2025-08-07 00:37:01 +0500

    Update simulation2.js
    fixed the script

--- experiments/of0/rpl/simulation2.js
+++ experiments/of0/rpl/simulation2.js
@@ -1,9 +1,5 @@
 log.log("Simulation started\n");
-
-GENERATE_MSG(60000, "timeout");
-
-YIELD_THEN_WAIT_UNTIL(msg.equals("timeout"));
-
-log.log("Timeout reached. Stopping simulation.\n");
-
-log.testOK(); // Report test success and quit
+TIMEOUT(60000);
+YIELD(); // Wait until TIMEOUT is triggered
+log.log("Simulation reached 60 seconds. Ending.\n");
+log.testOK();
