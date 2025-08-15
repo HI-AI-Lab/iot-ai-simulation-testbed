f183403c46c5f554df5ded7e4fbfdee98e1e080c
Mufrah Naveed <mufrah.naveed.mughal@gmail.com>
2025-08-15 20:00:43 +0500

    Update simulation4.js
    well another try

--- experiments/of0/rpl/simulation4.js
+++ experiments/of0/rpl/simulation4.js
@@ -5,11 +5,11 @@ var DURATION_MS = 60000; // 60 seconds
 TIMEOUT(DURATION_MS, log.testOK());
 
 // Optional: note start in COOJA.testlog
-log.log("Logging all mote output...\n");
+//log.log("Logging all mote output...\n");
 
 // Capture every mote line exactly as COOJA provides it.
 // 'id' = mote id, 'time' = sim time (ms), 'msg' = raw string shown in GUI.
-while (true) {
-  YIELD(); // wait for next mote output
-  log.log("ID:" + id + "  TIME:" + time + "  MSG:" + msg + "\n");
-}
+//while (true) {
+//  YIELD(); // wait for next mote output
+//  log.log("ID:" + id + "  TIME:" + time + "  MSG:" + msg + "\n");
+//}
