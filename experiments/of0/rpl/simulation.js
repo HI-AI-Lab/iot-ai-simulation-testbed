4f190f307b99b0d3da691cc7f752ffcb7de621fc
Mufrah Naveed <mufrah.naveed.mughal@gmail.com>
2025-08-17 21:06:56 +0500

    Update simulation.js
    updated JS

--- experiments/of0/rpl/simulation.js
+++ experiments/of0/rpl/simulation.js
@@ -1,8 +1,11 @@
-var RUN_DURATION_MS = 300000;
+// simulation.js
+var RUN_DURATION_MS = 300000; // 5 minutes
 
+// End the test after RUN_DURATION_MS and mark it as success
 TIMEOUT(RUN_DURATION_MS, log.testOK());
 
+// Main loop: print all mote output to the COOJA log
 while (true) {
-  YIELD();
+  YIELD(); // wait for a new mote message
   log.log(time + "\t" + id + "\t" + msg + "\n");
 }
