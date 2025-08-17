9586ced5fb0934ccf6a63dff9fcc63e7bfa4f015
Mufrah Naveed <mufrah.naveed.mughal@gmail.com>
2025-08-17 21:01:36 +0500

    Update simulation.js
    removed comments

--- experiments/of0/rpl/simulation.js
+++ experiments/of0/rpl/simulation.js
@@ -1,11 +1,8 @@
-// Run simulation for exactly this many milliseconds of simulated time
-var run_duration_ms = 300000; // 5 minutes
+var RUN_DURATION_MS = 300000;
 
-TIMEOUT(300000, log.testOK()); // On timeout, exit with status 0
+TIMEOUT(RUN_DURATION_MS, log.testOK());
 
 while (true) {
   YIELD();
-  // Append every mote output to the simulator log
-  // Variables provided: time (ms), id (mote id), msg (string)
   log.log(time + "\t" + id + "\t" + msg + "\n");
 }
