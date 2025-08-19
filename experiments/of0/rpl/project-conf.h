4f137a208e60a9bcbbf5e1ba2fe82dc4e2da45df
Mufrah Naveed <mufrah.naveed.mughal@gmail.com>
2025-08-19 13:36:17 +0500

    Update project-conf.h
    OF0 update

--- experiments/of0/rpl/project-conf.h
+++ experiments/of0/rpl/project-conf.h
@@ -1,26 +1,24 @@
 #ifndef PROJECT_CONF_H_
 #define PROJECT_CONF_H_
 
-/* ----- Logging / IPv6 ----- */
-#undef LOG_WITH_COMPACT_ADDR
-#define LOG_WITH_COMPACT_ADDR 0
+/* Use RPL Lite */
+#undef NETSTACK_CONF_ROUTING
+#define NETSTACK_CONF_ROUTING rpl_lite_driver
+
+/* Objective Function: OF0 (hop count) */
+#undef RPL_CONF_OF
+#define RPL_CONF_OF rpl_of0
+
+/* Don’t send RAs from nodes */
 #undef UIP_CONF_ND6_SEND_RA
 #define UIP_CONF_ND6_SEND_RA 0
 
+/* Logging */
+#undef LOG_WITH_COMPACT_ADDR
+#define LOG_WITH_COMPACT_ADDR 0
 #define LOG_CONF_LEVEL_RPL    LOG_LEVEL_INFO
 #define LOG_CONF_LEVEL_IPV6   LOG_LEVEL_WARN
 #define LOG_CONF_LEVEL_TCPIP  LOG_LEVEL_WARN
 #define LOG_CONF_LEVEL_APP    LOG_LEVEL_INFO
-
-/* ----- Routing: RPL Classic + OF0 (hop-count) ----- */
-#undef NETSTACK_CONF_ROUTING
-#define NETSTACK_CONF_ROUTING rpl_classic_driver
-
-#undef RPL_CONF_OF
-#define RPL_CONF_OF rpl_of0
-
-/* (optional but tidy) pin OCP to OF0 */
-#undef RPL_CONF_OF_OCP
-#define RPL_CONF_OF_OCP RPL_OCP_OF0
 
 #endif /* PROJECT_CONF_H_ */
