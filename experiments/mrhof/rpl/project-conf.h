0e19d205cd70e9d49ca46bb511167b7142653915
Mufrah Naveed <mufrah.naveed.mughal@gmail.com>
2025-08-19 14:07:18 +0500

    added directroy for mrhof

---
+++ experiments/mrhof/rpl/project-conf.h
@@ -0,0 +1,24 @@
+#ifndef PROJECT_CONF_H_
+#define PROJECT_CONF_H_
+
+/* Use RPL Lite */
+#undef NETSTACK_CONF_ROUTING
+#define NETSTACK_CONF_ROUTING rpl_lite_driver
+
+/* Objective Function: OF0 (hop count) */
+#undef RPL_CONF_OF
+#define RPL_CONF_OF rpl_of0
+
+/* Don’t send RAs from nodes */
+#undef UIP_CONF_ND6_SEND_RA
+#define UIP_CONF_ND6_SEND_RA 0
+
+/* Logging */
+#undef LOG_WITH_COMPACT_ADDR
+#define LOG_WITH_COMPACT_ADDR 0
+#define LOG_CONF_LEVEL_RPL    LOG_LEVEL_INFO
+#define LOG_CONF_LEVEL_IPV6   LOG_LEVEL_WARN
+#define LOG_CONF_LEVEL_TCPIP  LOG_LEVEL_WARN
+#define LOG_CONF_LEVEL_APP    LOG_LEVEL_INFO
+
+#endif /* PROJECT_CONF_H_ */
