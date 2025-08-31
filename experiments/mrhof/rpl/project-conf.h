7281e105c24c16e6e582622ee2139fda7c74a1bf
Mufrah Naveed <mufrah.naveed.mughal@gmail.com>
2025-08-31 20:28:24 +0500

    fixed mrhof

--- experiments/mrhof/rpl/project-conf.h
+++ experiments/mrhof/rpl/project-conf.h
@@ -1,23 +1,28 @@
 #ifndef PROJECT_CONF_H_
 #define PROJECT_CONF_H_
 
-/* Select RPL MRHOF (ETX-based) */
+/* Use RPL Lite */
+#undef NETSTACK_CONF_ROUTING
+#define NETSTACK_CONF_ROUTING rpl_lite_driver
+
+/* Select Objective Function: MRHOF */
+#undef RPL_CONF_OF_OCP
 #define RPL_CONF_OF_OCP RPL_OCP_MRHOF
 
-/* Enable RPL Lite */
-#define UIP_CONF_ROUTER              1
-#define NETSTACK_CONF_ROUTING        rpl_classic
+/* Router mode */
+#undef UIP_CONF_ROUTER
+#define UIP_CONF_ROUTER 1
 
-/* Configure Queue Buffer / Energest for metrics */
-#define QUEUEBUF_CONF_NUM            16
-#define ENERGEST_CONF_ON             1
+/* Optional tuning */
+#define QUEUEBUF_CONF_NUM 16
+#define ENERGEST_CONF_ON  1
 
 /* Logging */
-#define LOG_CONF_LEVEL_RPL           LOG_LEVEL_INFO
-#define LOG_CONF_LEVEL_IPV6          LOG_LEVEL_INFO
-#define LOG_CONF_LEVEL_TCPIP         LOG_LEVEL_WARN
-#define LOG_CONF_LEVEL_MAC           LOG_LEVEL_WARN
-#define LOG_CONF_LEVEL_FRAMER        LOG_LEVEL_WARN
-#define LOG_CONF_LEVEL_6LOWPAN       LOG_LEVEL_WARN
+#define LOG_CONF_LEVEL_RPL     LOG_LEVEL_INFO
+#define LOG_CONF_LEVEL_IPV6    LOG_LEVEL_INFO
+#define LOG_CONF_LEVEL_TCPIP   LOG_LEVEL_WARN
+#define LOG_CONF_LEVEL_MAC     LOG_LEVEL_WARN
+#define LOG_CONF_LEVEL_FRAMER  LOG_LEVEL_WARN
+#define LOG_CONF_LEVEL_6LOWPAN LOG_LEVEL_WARN
 
 #endif /* PROJECT_CONF_H_ */
