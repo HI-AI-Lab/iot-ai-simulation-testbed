38ef54591f4668ad667ad004e2c066b222bb634f
Mufrah Naveed <mufrah.naveed.mughal@gmail.com>
2025-09-19 09:01:20 +0500

    going back to the basics

--- experiments/of0/node.c
+++ experiments/of0/node.c
@@ -3,51 +3,91 @@
 #include "random.h"
 #include "net/netstack.h"
 #include "net/ipv6/simple-udp.h"
-#include "sys/log.h"
-#include "sys/node-id.h"
-#include <stdio.h>
-#include <string.h>
+#include <stdint.h>
+#include <inttypes.h>
 
+#include "sys/log.h"
 #define LOG_MODULE "App"
 #define LOG_LEVEL LOG_LEVEL_INFO
 
-#define UDP_CLIENT_PORT 8765
-#define UDP_SERVER_PORT 5678
+#define WITH_SERVER_REPLY  1
+#define UDP_CLIENT_PORT	8765
+#define UDP_SERVER_PORT	5678
+
+#define SEND_INTERVAL		  (10 * CLOCK_SECOND)
 
 static struct simple_udp_connection udp_conn;
-static struct etimer periodic_timer;
-static unsigned long seqno = 0;
+static uint32_t rx_count = 0;
 
-PROCESS(udp_client_process, "UDP client");
+/*---------------------------------------------------------------------------*/
+PROCESS(udp_client_process, "NODE");
 AUTOSTART_PROCESSES(&udp_client_process);
+/*---------------------------------------------------------------------------*/
+static void
+udp_rx_callback(struct simple_udp_connection *c,
+         const uip_ipaddr_t *sender_addr,
+         uint16_t sender_port,
+         const uip_ipaddr_t *receiver_addr,
+         uint16_t receiver_port,
+         const uint8_t *data,
+         uint16_t datalen)
+{
 
+  LOG_INFO("Received response '%.*s' from ", datalen, (char *) data);
+  LOG_INFO_6ADDR(sender_addr);
+#if LLSEC802154_CONF_ENABLED
+  LOG_INFO_(" LLSEC LV:%d", uipbuf_get_attr(UIPBUF_ATTR_LLSEC_LEVEL));
+#endif
+  LOG_INFO_("\n");
+  rx_count++;
+}
+/*---------------------------------------------------------------------------*/
 PROCESS_THREAD(udp_client_process, ev, data)
 {
-  uip_ipaddr_t root_ipaddr;
+  static struct etimer periodic_timer;
+  static char str[32];
+  uip_ipaddr_t dest_ipaddr;
+  static uint32_t tx_count;
+  static uint32_t missed_tx_count;
+
   PROCESS_BEGIN();
 
-  simple_udp_register(&udp_conn, UDP_CLIENT_PORT, NULL, UDP_SERVER_PORT, NULL);
+  /* Initialize UDP connection */
+  simple_udp_register(&udp_conn, UDP_CLIENT_PORT, NULL,
+                      UDP_SERVER_PORT, udp_rx_callback);
 
-  /* Random startup delay */
-  etimer_set(&periodic_timer, random_rand() % (3 * CLOCK_SECOND));
-
+  etimer_set(&periodic_timer, random_rand() % SEND_INTERVAL);
   while(1) {
     PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&periodic_timer));
 
     if(NETSTACK_ROUTING.node_is_reachable() &&
-       NETSTACK_ROUTING.get_root_ipaddr(&root_ipaddr)) {
+        NETSTACK_ROUTING.get_root_ipaddr(&dest_ipaddr)) {
 
-      char buf[32];
-      snprintf(buf, sizeof(buf), "node=%u;seq=%lu", node_id, seqno);
+      /* Print statistics every 10th TX */
+      if(tx_count % 10 == 0) {
+        LOG_INFO("Tx/Rx/MissedTx: %" PRIu32 "/%" PRIu32 "/%" PRIu32 "\n",
+                 tx_count, rx_count, missed_tx_count);
+      }
 
-      LOG_INFO("TX\tnode=%u\tseq=%lu\n", node_id, seqno);
-
-      simple_udp_sendto(&udp_conn, buf, strlen(buf), &root_ipaddr);
-      seqno++;
+      /* Send to DAG root */
+      LOG_INFO("Sending request %"PRIu32" to ", tx_count);
+      LOG_INFO_6ADDR(&dest_ipaddr);
+      LOG_INFO_("\n");
+      snprintf(str, sizeof(str), "hello %" PRIu32 "", tx_count);
+      simple_udp_sendto(&udp_conn, str, strlen(str), &dest_ipaddr);
+      tx_count++;
+    } else {
+      LOG_INFO("Not reachable yet\n");
+      if(tx_count > 0) {
+        missed_tx_count++;
+      }
     }
 
-    etimer_set(&periodic_timer, (clock_time_t)(SEND_INTERVAL_SEC * CLOCK_SECOND));
+    /* Add some jitter */
+    etimer_set(&periodic_timer, SEND_INTERVAL
+      - CLOCK_SECOND + (random_rand() % (2 * CLOCK_SECOND)));
   }
 
   PROCESS_END();
 }
+/*---------------------------------------------------------------------------*/
