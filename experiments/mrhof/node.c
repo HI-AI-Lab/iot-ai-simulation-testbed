e13364d785bccca9eb4a7a80edcf70c22a721541
Mufrah Naveed <mufrah.naveed.mughal@gmail.com>
2025-09-19 13:32:43 +0500

    removed echo from server and recieve call back from ordinary nodes

--- experiments/mrhof/node.c
+++ experiments/mrhof/node.c
@@ -10,7 +10,6 @@
 #define LOG_MODULE "App"
 #define LOG_LEVEL LOG_LEVEL_INFO
 
-#define WITH_SERVER_REPLY  1
 #define UDP_CLIENT_PORT	8765
 #define UDP_SERVER_PORT	5678
 
@@ -23,24 +22,6 @@ static uint32_t rx_count = 0;
 PROCESS(udp_client_process, "NODE");
 AUTOSTART_PROCESSES(&udp_client_process);
 /*---------------------------------------------------------------------------*/
-static void
-udp_rx_callback(struct simple_udp_connection *c,
-         const uip_ipaddr_t *sender_addr,
-         uint16_t sender_port,
-         const uip_ipaddr_t *receiver_addr,
-         uint16_t receiver_port,
-         const uint8_t *data,
-         uint16_t datalen)
-{
-
-  LOG_INFO("Received response '%.*s' from ", datalen, (char *) data);
-  LOG_INFO_6ADDR(sender_addr);
-#if LLSEC802154_CONF_ENABLED
-  LOG_INFO_(" LLSEC LV:%d", uipbuf_get_attr(UIPBUF_ATTR_LLSEC_LEVEL));
-#endif
-  LOG_INFO_("\n");
-  rx_count++;
-}
 /*---------------------------------------------------------------------------*/
 PROCESS_THREAD(udp_client_process, ev, data)
 {
@@ -54,7 +35,7 @@ PROCESS_THREAD(udp_client_process, ev, data)
 
   /* Initialize UDP connection */
   simple_udp_register(&udp_conn, UDP_CLIENT_PORT, NULL,
-                      UDP_SERVER_PORT, udp_rx_callback);
+                      UDP_SERVER_PORT, NULL);
 
   etimer_set(&periodic_timer, random_rand() % SEND_INTERVAL);
   while(1) {
