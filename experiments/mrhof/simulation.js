d8a26915e67d587f8d1c9f5609ccccdbf0072531
Mufrah Naveed <mufrah.naveed.mughal@gmail.com>
2025-09-23 17:22:56 +0500

    added 1 time ping png

--- experiments/mrhof/simulation.js
+++ experiments/mrhof/simulation.js
@@ -6,6 +6,26 @@ var BufferedReader = Java.type("java.io.BufferedReader");
 
 TIMEOUT(6000000, log.testOK()); // On timeout, exit with status 0
 
+// --- One-shot ping at simulation start ---
+try {
+  var sock = new Socket("localhost", 5000);
+  var out = new PrintWriter(sock.getOutputStream(), true);
+  var inp = new BufferedReader(new InputStreamReader(sock.getInputStream()));
+
+  var msg = "PING";
+  log.log("JS sending: " + msg + "\n");
+  out.println(msg);
+
+  var line = inp.readLine();
+  if (line != null) {
+    log.log("JS got reply: " + line + "\n");
+  }
+
+  sock.close();
+} catch (e) {
+  log.log("Socket error: " + e + "\n");
+}
+
 while (true) {
   YIELD();
   // Append every mote output to the simulator log
