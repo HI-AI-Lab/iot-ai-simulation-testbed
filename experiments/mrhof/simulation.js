bcd54f20d0036680d5106c9adf5a6debaf12e8dd
Mufrah Naveed <mufrah.naveed.mughal@gmail.com>
2025-09-24 18:21:49 +0500

    investigate

--- experiments/mrhof/simulation.js
+++ experiments/mrhof/simulation.js
@@ -17,7 +17,8 @@ try {
     // --- Main simulation loop ---
     while (true) {
         YIELD();
-		log.log("[JS] mote class = " + mote.getMemory().getNumberOfSections()+ "\n");
+		log.log("[JS] Mote Class = "+mote.getClass().getName()+"\n");
+		//log.log("[JS] mote class = " + mote.getMemory().getNumberOfSections()+ "\n");
 		//var mem = mote.getMemory();
 		//var toggle_value = mem.getInt32ValueOf("toggle_value");
 		//toggle_value = toggle_value+1;
