3fba62fb5d2ec3edacbf3d7a691048df59ff55bc
Mufrah Naveed <mufrah.naveed.mughal@gmail.com>
2025-09-24 18:49:48 +0500

    Update simulation.js

--- experiments/mrhof/simulation.js
+++ experiments/mrhof/simulation.js
@@ -23,6 +23,7 @@ try {
 		log.log("[JS] Class = "+mote.getMemory().getSections().keySet().getClass().getName()+"\n");
 		log.log("[JS] Class = "+mote.getMemory().getSections().keySet().size()+"\n");
 		log.log("[JS] Class = "+mote.getMemory().getSections().keySet().toArray()[0]+"\n");
+		log.log("[JS] Class = "+mote.getMemory().getSections().keySet().toArray()[1]+"\n");
 		//log.log("[JS] Class = "+mote.getMemory().getSections().keySet().toArray()[0].toString()"\n");
 		//log.log("[JS] mote class = " + mote.getMemory().getNumberOfSections()+ "\n");
 		//var mem = mote.getMemory();
