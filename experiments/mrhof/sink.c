5da8498581675a2016e27b88b597cdc7420008f2
Mufrah Naveed <mufrah.naveed.mughal@gmail.com>
2025-09-24 14:55:55 +0500

    introduced toggle in sink and nodes

--- experiments/mrhof/sink.c
+++ experiments/mrhof/sink.c
@@ -64,7 +64,7 @@ static node_stats_t stats[NUM_NODES+1];   // index 0 dummy, 1 sink, 2..N motes
 
 static void
 wrapup(void) {
-  LOG_INFO("TOGGLE_STATUS OF SINK toggle_value=%d", toggle_value);
+  LOG_INFO("TOGGLE_STATUS OF SINK toggle_value=%d\n", toggle_value);
   LOG_INFO("WRAPUP sink end_ms=%"PRIu32"\n",
            (uint32_t)(clock_time() * 1000UL / CLOCK_SECOND));
 
