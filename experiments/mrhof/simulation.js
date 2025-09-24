9ca915edc74dcdb202d6dce46b95274738c62b2a
Mufrah Naveed <mufrah.naveed.mughal@gmail.com>
2025-09-24 18:44:17 +0500

    investigating memory

--- experiments/mrhof/simulation.js
+++ experiments/mrhof/simulation.js
@@ -21,6 +21,7 @@ try {
 		log.log("[JS] Class = "+mote.getMemory().getClass().getName()+"\n");
 		log.log("[JS] Class = "+mote.getMemory().getSections().getClass().getName()+"\n");
 		log.log("[JS] Class = "+mote.getMemory().getSections().keySet().getClass().getName()+"\n");
+		log.log("[JS] Class = "+mote.getMemory().getSections().keySet().size()+"\n");
 		//log.log("[JS] Class = "+mote.getMemory().getSections().keySet().toArray()[0].toString()"\n");
 		//log.log("[JS] mote class = " + mote.getMemory().getNumberOfSections()+ "\n");
 		//var mem = mote.getMemory();
